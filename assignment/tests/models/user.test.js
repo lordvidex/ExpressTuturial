b421d227fd851fae28b1b01938c687ab66e13f90
lordvidex <lordvidex.prime@gmail.com>
2021-04-25 02:32:12 +0300

    Added unit test to user generateauthtoken method

---
+++ assignment/tests/models/user.test.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const mongoose = require("mongoose")
+const { User } = require("../../models/user")
+
+describe('Generating UserAuthToken',()=>{
+    test('should return a valid token',()=>{
+        // Given
+        const payload = {
+            _id: mongoose.Types.ObjectId().toHexString(),
+            isAdmin: true
+        };
+        const user = new User(payload);
+
+        // When
+        const token = user.generateAuthToken();
+        
+        // Then
+        expect(token).not.toBeUndefined();
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        expect(decoded).toMatchObject(payload);
+    });
+})
